2653a58ebeeaf4381d727fc55b5e0ea112dba861
hydrobee3000 <hydrombee@gmail.com>
2021-11-09 13:21:43 +0600

    absolutely correct display on mobile)

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -23,9 +23,9 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
-    height: '100px',
+    height: '115px',
   },
-  height: '100px',
+  height: '115px',
 
   // hide last border
   '&:last-child td, &:last-child th': {
