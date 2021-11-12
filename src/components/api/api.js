c1344d96e2d4aefd7ff44242576407fb3f673ec8
hydrobee3000 <hydrombee@gmail.com>
2021-11-12 14:11:43 +0600

    removed repeat part of url in api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,23 +1,21 @@
 import * as axios from 'axios'
 
 const instance = axios.create({
-  baseURL: `https://free.currconv.com/api/v7/`,
+  baseURL: `https://api.fastforex.io/`,
   headers: { Accept: 'application/json' },
 })
 
 export const currencyAPI = {
   //получить все валюты
   getAllCurrencies() {
-    return instance.get(`https://api.fastforex.io/currencies?api_key=8df75b22c9-558651ffe1-r2e4ku`)
+    return instance.get(`currencies?api_key=8df75b22c9-558651ffe1-r2e4ku`)
   },
   //получить ставку относительно базовой валюты
   getRateFromBaseCurrencies(baseCurrency) {
-    return instance.get(`https://api.fastforex.io/fetch-all?from=${baseCurrency}&api_key=8df75b22c9-558651ffe1-r2e4ku`)
+    return instance.get(`fetch-all?from=${baseCurrency}&api_key=8df75b22c9-558651ffe1-r2e4ku`)
   },
   // конвертировать
   getResult(baseCurrency, toCurrency, amount) {
-    return instance.get(
-      `https://api.fastforex.io/convert?from=${baseCurrency}&to=${toCurrency}&amount=${amount}&api_key=8df75b22c9-558651ffe1-r2e4ku`
-    )
+    return instance.get(`convert?from=${baseCurrency}&to=${toCurrency}&amount=${amount}&api_key=8df75b22c9-558651ffe1-r2e4ku`)
   },
 }
